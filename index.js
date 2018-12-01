74663d606ff298f8a0ffb6e9b09d3c84acfaf7aa
lTyl <tderen@aurava.com>
2018-12-01 15:51:22 -0700

    1.0.1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const output = argv["output"] || './patch-file.pak';
 
 
 var bundle = new wb.Bundle();
-bundle.addFile('./patch-file.js', function(err) {
+bundle.addFile('patch-file.js', function(err) {
 	bundle.write(output, function(err) {
 		if (err){
 			console.log("ERROR", err);
